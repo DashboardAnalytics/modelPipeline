f16b91da34a5d972faec6a0d69e7dc58d1582308
GarsePersen <sebastian.garay.p@usach.cl>
2019-12-09 16:13:22 -0300

    Download blob implemented

---
+++ DownloadBlobs.py
@@ -0,0 +1,19 @@
+from google.cloud import storage
+
+bucketName = "streamed-videos"
+
+# Get elements name
+def download(bucketName, storageClient):
+    storageClient = storage.Client()
+    bucket = storageClient.get_bucket(bucketName)
+    blobs = storageClient.list_blobs(bucketName)
+    for blob in blobs:
+        print("Downloading blob:", blob.name)
+        blob.download_to_filename(fileDestinationName)
+
+    return True
+
+
+
+if(download(bucketName)):
+    print("Downloads complete!")
